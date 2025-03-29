1d53558d3c33373be132dffe70378d42c9a77362
Navneet Sahota <sahota.navneet04@gmail.com>
2025-03-29 14:58:23 +0530

    Update import path alias

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,13 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      "eslint-config-prettier",
-    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -28,5 +26,6 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  }
+  },
+  prettier
 );
